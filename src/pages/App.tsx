d45f02ea91129588e3f605e9e59213fc7de4a16d
Darcio Vilela <darcio@jp7.com.br>
2021-09-16 12:22:15 -0300

    Remove unnecessary files

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header>InterMail</header>
+    </div>
+  );
+};
+
+export default App;
