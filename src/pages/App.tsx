789ffb1d527e715a43972e599196cac425c4a219
Darcio Vilela <darcio@jp7.com.br>
2021-09-16 19:32:32 -0300

    Adjustments in page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 
 export const App: React.FC = () => {
   return (
-    <form>
-      <h1>Email Sender</h1>
-      <p>To:</p>
-      <input type="text" />
-      <p>Subject:</p>
-      <input type="text" />
-      <p>Message:</p>
-      <input type="text" />
-      <br />
-      <button>Send</button>
-    </form>
+    <div className="center">
+      <form>
+        <h1>Email Sender</h1>
+        <p>To:</p>
+        <input type="text" />
+        <p>Subject:</p>
+        <input type="text" />
+        <p>Message:</p>
+        <input type="text" />
+        <br />
+        <button>Send</button>
+      </form>
+    </div>
   );
 };
 
