6f37c50a0b3366ad5b7a7e0a621b63f9bc9014d5
Darcio Vilela <darcio@jp7.com.br>
2021-11-24 20:16:46 -0300

    Update form with useState

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Component
 const Form = () => {
+  const [campos, setCampos] = useState({
+    to: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleInputChange = (event: { target: { name: any; value: any } }) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setCampos((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleFormSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    alert(campos);
+  };
+
   return (
     <div className="form">
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <h1>Email Sender</h1>
         <label>To:</label>
-        <input name="to" type="text" />
+        <input
+          name="to"
+          type="text"
+          value={campos.to}
+          onChange={handleInputChange}
+        />
         <label>Subject:</label>
-        <input name="subject" type="text" />
+        <input
+          name="subject"
+          type="text"
+          value={campos.subject}
+          onChange={handleInputChange}
+        />
         <label>Message:</label>
-        <input name="message" type="text" />
+        <input
+          name="message"
+          type="text"
+          value={campos.message}
+          onChange={handleInputChange}
+        />
         <input className="submit" type="submit" />
       </form>
     </div>
